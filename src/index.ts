91f9356bddc1f6c1fc5341a935accb54cbee1886
Sarthak Jha <sarthakjha2001@gmail.com>
2020-07-25 00:34:07 +0530

    additional config

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+interface Colors {
+  R: number;
+  G: number;
+  B: number;
+}
+
+const color: Colors = {
+  R: 100,
+  G: 12,
+  B: 23,
+};
+
+console.log(color);
+
+//to use this in other files
+export default color;
